5654ec00989b8cfdb25a02d7b188006d307ac6e0
thkaza <thkaza@gmail.com>
2015-12-27 02:32:48 +0200

    sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,8 @@
-﻿select idDwmatiou,arithmosDwmatiou 
-                                                          from ROOMS
-                                                         where idTyposDwmatiou = '2'
-                                                           and idDwmatiou
-                                                        NOT IN (select idDwmatiou from KRATISEIS 
-                                                                 where (hmerominiaAfixis <= '5-12-2015' and hmerominiaAnaxwrisis > '5-12-2015')
-                                                                   OR (hmerominiaAfixis<'8-12-2015' AND hmerominiaAnaxwrisis>='8-12-2015')
-                                                                   OR (hmerominiaAfixis>='5-12-2015' and hmerominiaAfixis<='8-12-2015'))
-                                                            OR (idDwmatiou = '1')
+﻿select r.arithmosDwmatiou, rt.perigrafi, tk.perigrafi, datediff(day,kr.hmerominiaAfixis, kr.hmerominiaAnaxwrisis) daysdiff
+  from ROOMS r 
+  left join ROOMTYPE rt
+         on r.idDwmatiou = rt.idTyposDwmatiou
+  left join KRATISEIS kr
+         on kr.idDwmatiou = r.idDwmatiou
+  left join TyposKratisis tk
+         on tk.idTyposKratisis = kr.idTyposKratisis
